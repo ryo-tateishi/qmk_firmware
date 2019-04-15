680bc6cf96bcd897815880b7593ca213376da576
monksoffunk <osamu@sho-k.com>
2019-04-15 19:45:34 +0900

    Add zinc_ble

---
+++ keyboards/zinc_ble/master/config.h
@@ -0,0 +1,11 @@
+#ifndef MASTER_CONFIG_H_
+#define MASTER_CONFIG_H_
+
+#include "custom_board.h"
+
+#define THIS_DEVICE_ROWS 4
+#define THIS_DEVICE_COLS 6
+
+#define IS_LEFT_HAND  true
+
+#endif /* MASTER_CONFIG_H_ */
---
+++ keyboards/zinc_ble/slave/config.h
@@ -0,0 +1,14 @@
+#ifndef SLAVE_CONFIG_H_
+#define SLAVE_CONFIG_H_
+
+#include "custom_board.h"
+
+//#define THIS_DEVICE_ROWS 4
+//#define MATRIX_ROW_PINS { PIN7, PIN8, PIN9, PIN10 }
+
+//#define THIS_DEVICE_COLS 6
+//#define MATRIX_COL_PINS { PIN20, PIN19, PIN18, PIN17, PIN16, PIN15, PIN14 }
+
+#define IS_LEFT_HAND  false
+
+#endif /* SLAVE_CONFIG_H_ */
