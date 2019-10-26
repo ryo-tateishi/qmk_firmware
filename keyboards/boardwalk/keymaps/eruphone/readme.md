bc5b7979299471f4677fc91edf0f467ddaa426f7
eruphone <fez.bellerophon@gmail.com>
2019-10-26 18:04:27 +0900

    add boardwalk keymap for ryo-tateishi

---
+++ keyboards/boardwalk/keymaps/eruphone/readme.md
@@ -0,0 +1,1 @@
+# Eruphone keymap for Boardwalk
--- keyboards/boardwalk/rules.mk
+++ keyboards/boardwalk/rules.mk
@@ -26,7 +26,7 @@ MIDI_ENABLE = no            # MIDI support (+2400 to 4200, depending on config)
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = yes        # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 
 LAYOUTS = ortho_5x14
