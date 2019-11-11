e295dff212430d7714c963f87911a4e72cb65a2d
eruphone <fez.bellerophon@gmail.com>
2019-11-12 00:01:17 +0900

    makeのエラーを解消

--- keyboards/boardwalk/keymaps/eruphone/keymap.c
+++ keyboards/boardwalk/keymaps/eruphone/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
  * | TAB    |   Q    |   W    |   E    |   R    |   T    |   Y    |   U    |   I    |   O    |   P    |  @/`   |  [/{   | DELETE |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | CTRL   |   A    |   S    |   D    |   F    |   G    |   H    |   J    |   K    |   L    |  ;/+   |  :/*   |  ]/}   | BKSP   |
+ * | CTRL   |   A    |   S    |   D    |   F    |   G    |   H    |   J    |   K    |   L    |  ;/+   |  :/＊  |  ]/}   | BKSP   |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * | SHIFT  |   Z    |   X    |   C    |   V    |   B    |   N    |   M    |  ,/<   |  ./>   |  //?   | SHIFT  |   ↑    |   _    |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_AT   , KC_LBRC, KC_DEL , \
   KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_COLN , KC_RBRC, KC_BSPC, \
   KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT , KC_UP  , KC_UNDS, \
-  MO(FN) , KC_LCTL, KC_LGUI, KC_HENK,      KC_SPC     ,      KC_ENT     , KC_MHEN, KC_RGUI, MO(FN) , KC_LEFT , KC_DOWN, KC_RGHT  \
+  MO(_FN), KC_LCTL, KC_LGUI, KC_HENK,      KC_SPC     ,      KC_ENT     , KC_MHEN, KC_RGUI, MO(_FN), KC_LEFT , KC_DOWN, KC_RGHT  \
  ),
 
 /* FUNCTION
