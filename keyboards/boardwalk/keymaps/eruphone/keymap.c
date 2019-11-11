e479ad20c0cddcf6ace774ac245aed4e500567d6
eruphone <fez.bellerophon@gmail.com>
2019-11-11 23:50:07 +0900

    不要と思われるシフト時のキーマップを削除

--- keyboards/boardwalk/keymaps/eruphone/keymap.c
+++ keyboards/boardwalk/keymaps/eruphone/keymap.c
@@ -18,8 +18,7 @@
 // Layer shorthand
 enum layer {
   _DEFAULT,
-  _SHIFT,
-  _FN,
+  _FN
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -43,27 +42,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_COLN , KC_RBRC, KC_BSPC, \
   KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT , KC_UP  , KC_UNDS, \
   MO(FN) , KC_LCTL, KC_LGUI, KC_HENK,      KC_SPC     ,      KC_ENT     , KC_MHEN, KC_RGUI, MO(FN) , KC_LEFT , KC_DOWN, KC_RGHT  \
- ),
-
- /* SHIFT
- * .-----------------------------------------------------------------------------------------------------------------------------.
- * | ESC    |  1/!   |  2/"   |  3/#   |  4/$   |  5/%   |  6/&   |  7/'   |  8/(   |  9/)   |   0    |  -/=   |  ^/~   | BKSP   |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | TAB    |   Q    |   W    |   E    |   R    |   T    |   Y    |   U    |   I    |   O    |   P    |  @/`   |  [/{   | ENTER  |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | CTRL   |   A    |   S    |   D    |   F    |   G    |   H    |   J    |   K    |   L    |  ;/+   |  :/*   |  ]/}   |  ¥/|   |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | SHIFT  |   Z    |   X    |   C    |   V    |   B    |   N    |   M    |  ,/<   |  ./>   |  //?   |   _    |   ↑    | DELETE |
- * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
- * | OPT    | COMMAND    | EN     |      SPACE      |      SPACE      |  KANA  | COMMAND     |  FN    |   ←    |   ↓    |   →    |
- * '-----------------------------------------------------------------------------------------------------------------------------'
- */
- [_SHIFT] = LAYOUT_ortho_5x14(
-  KC_NO  ,  KC_EXLM,  KC_DQUO,  KC_HASH,  KC_DLR ,  KC_PERC,  KC_AMPR,  KC_QUOT,  KC_LPRN,  KC_RPRN,  KC_NO  ,  KC_EQL ,  KC_TILD,  KC_NO  , \
-  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_GRV ,  KC_LCBR,  KC_NO  , \
-  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_PLUS,  KC_ASTR,  KC_RCBR,  KC_PIPE, \
-  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_LABK,  KC_RABK,  KC_QUES,  KC_NO  ,  KC_NO  ,  KC_NO  , \
-  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO    \
  ),
 
 /* FUNCTION
