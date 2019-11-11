8fa584fa110b01415a5bfe6fd5d48da8c6e60813
eruphone <fez.bellerophon@gmail.com>
2019-11-11 23:48:59 +0900

    デフォルトキーマップを変更し、コードフォーマットを改善

--- keyboards/boardwalk/keymaps/eruphone/keymap.c
+++ keyboards/boardwalk/keymaps/eruphone/keymap.c
@@ -26,23 +26,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* MAIN
  * .-----------------------------------------------------------------------------------------------------------------------------.
- * | ESC    |  1/!   |  2/"   |  3/#   |  4/$   |  5/%   |  6/&   |  7/'   |  8/(   |  9/)   |   0    |  -/=   |  ^/~   | BKSP   |
+ * | ESC    |  1/!   |  2/"   |  3/#   |  4/$   |  5/%   |  6/&   |  7/'   |  8/(   |  9/)   |   0    |  -/=   |  ^/~   |  ¥/|   |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | TAB    |   Q    |   W    |   E    |   R    |   T    |   Y    |   U    |   I    |   O    |   P    |  @/`   |  [/{   | ENTER  |
+ * | TAB    |   Q    |   W    |   E    |   R    |   T    |   Y    |   U    |   I    |   O    |   P    |  @/`   |  [/{   | DELETE |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | CTRL   |   A    |   S    |   D    |   F    |   G    |   H    |   J    |   K    |   L    |  ;/+   |  :/*   |  ]/}   |  ¥/|   |
+ * | CTRL   |   A    |   S    |   D    |   F    |   G    |   H    |   J    |   K    |   L    |  ;/+   |  :/*   |  ]/}   | BKSP   |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | SHIFT  |   Z    |   X    |   C    |   V    |   B    |   N    |   M    |  ,/<   |  ./>   |  //?   |   _    |   ↑    | DELETE |
+ * | SHIFT  |   Z    |   X    |   C    |   V    |   B    |   N    |   M    |  ,/<   |  ./>   |  //?   | SHIFT  |   ↑    |   _    |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
- * | OPT    | COMMAND    | EN     |      SPACE      |      SPACE      |  KANA  | COMMAND     |  FN    |   ←    |   ↓    |   →    |
+ * | FN     | OPT    | COMMAND| EN     |      SPACE      |      ENTER      |  KANA  | COMMAND|  FN    |   ←    |   ↓    |   →    |
  * '-----------------------------------------------------------------------------------------------------------------------------'
  */
  [_DEFAULT] = LAYOUT_ortho_5x14(
-  KC_ESC,   KC_1,     KC_2,     KC_3,    KC_4,  KC_5,    KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_MINUS, KC_CIRC,  KC_BSPC, \
-  KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,  KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_AT,     KC_LBRC,  KC_ENT, \
-  KC_LCTL,  KC_A,     KC_S,     KC_D,    KC_F,  KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_COLN,   KC_RBRC,  KC_JYEN  \
-  KC_LSFT,  KC_Z,     KC_X,     KC_C,    KC_V,  KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_UNDS,   KC_UP  ,  KC_DEL\
-  KC_LCTL,  KC_LGUI,  KC_HENK,  KC_SPC       ,  KC_SPC         ,   KC_MHEN,  KC_RGUI, MO(FN),            KC_LEFT, KC_DOWN,  KC_RGHT  \
+  KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINUS, KC_CIRC, KC_JYEN, \
+  KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_AT   , KC_LBRC, KC_DEL , \
+  KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_COLN , KC_RBRC, KC_BSPC, \
+  KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT , KC_UP  , KC_UNDS, \
+  MO(FN) , KC_LCTL, KC_LGUI, KC_HENK,      KC_SPC     ,      KC_ENT     , KC_MHEN, KC_RGUI, MO(FN) , KC_LEFT , KC_DOWN, KC_RGHT  \
  ),
 
  /* SHIFT
