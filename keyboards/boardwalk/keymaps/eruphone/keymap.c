2f1a72cd4cf3a216d48ec338ed4281abc1de30ad
ryo-tateishi <ryo-tateishi@bm-sms.co.jp>
2019-11-21 16:33:18 +0900

    Optionキーがなかった

--- keyboards/boardwalk/keymaps/eruphone/keymap.c
+++ keyboards/boardwalk/keymaps/eruphone/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB , KC_Q   , KC_W   , KC_E    , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I    , KC_O   , KC_P   , JP_AT   , JP_LBRC, JP_YEN , \
   KC_LCTL, KC_A   , KC_S   , KC_D    , KC_F   , KC_G   , KC_H   , KC_J   , KC_K    , KC_L   , KC_SCLN, JP_COLN , JP_RBRC, KC_ENT , \
   KC_LSFT, KC_Z   , KC_X   , KC_C    , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM , KC_DOT , KC_SLSH, KC_RSFT , KC_UP  , JP_BSLS, \
-  MO(_FN), KC_LCTL, KC_LGUI, JP_MEISU,      KC_SPC     ,      KC_ENT     , JP_MKANA, KC_RGUI, MO(_FN), KC_LEFT , KC_DOWN, KC_RGHT  \
+  MO(_FN), KC_LALT, KC_LGUI, JP_MEISU,      KC_SPC     ,      KC_ENT     , JP_MKANA, KC_RGUI, MO(_FN), KC_LEFT , KC_DOWN, KC_RGHT  \
  ),
 
 /* FUNCTION
