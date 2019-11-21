57fb1f19859aac5921f551b9dae61556d5db5219
eruphone <fez.bellerophon@gmail.com>
2019-11-21 09:57:41 +0900

    JIS対応

--- keyboards/boardwalk/keymaps/eruphone/keymap.c
+++ keyboards/boardwalk/keymaps/eruphone/keymap.c
@@ -2,6 +2,7 @@
  * Keymaps for Eruphone
  */
 #include QMK_KEYBOARD_H
+#include "keymap_jp.h"
 
 // Layer shorthand
 enum layer {
@@ -25,11 +26,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * '-----------------------------------------------------------------------------------------------------------------------------'
  */
  [_DEFAULT] = LAYOUT_eruphone(
-  KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINUS, KC_CIRC, KC_BSPC, \
-  KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_AT   , KC_LBRC, KC_JYEN, \
-  KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_COLN , KC_RBRC, KC_ENT , \
-  KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT , KC_UP  , KC_UNDS, \
-  MO(_FN), KC_LCTL, KC_LGUI, KC_HENK,      KC_SPC     ,      KC_ENT     , KC_MHEN, KC_RGUI, MO(_FN), KC_LEFT , KC_DOWN, KC_RGHT  \
+  KC_ESC , KC_1   , KC_2   , KC_3    , KC_4   , KC_5   , KC_6   , KC_7   , KC_8    , KC_9   , KC_0   , KC_MINUS, JP_CIRC, KC_BSPC, \
+  KC_TAB , KC_Q   , KC_W   , KC_E    , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I    , KC_O   , KC_P   , JP_AT   , JP_LBRC, JP_YEN , \
+  KC_LCTL, KC_A   , KC_S   , KC_D    , KC_F   , KC_G   , KC_H   , KC_J   , KC_K    , KC_L   , KC_SCLN, JP_COLN , JP_RBRC, KC_ENT , \
+  KC_LSFT, KC_Z   , KC_X   , KC_C    , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM , KC_DOT , KC_SLSH, KC_RSFT , KC_UP  , JP_BSLS, \
+  MO(_FN), KC_LCTL, KC_LGUI, JP_MEISU,      KC_SPC     ,      KC_ENT     , JP_MKANA, KC_RGUI, MO(_FN), KC_LEFT , KC_DOWN, KC_RGHT  \
  ),
 
 /* FUNCTION
