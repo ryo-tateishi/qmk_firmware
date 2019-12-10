75faca97dbe781d65f8927a5ea987f2fef968cd0
ryo-tateishi <ryo-tateishi@bm-sms.co.jp>
2019-12-10 17:39:16 +0900

    右シフトとアンダーバーの位置を入れ替える

--- keyboards/boardwalk/keymaps/eruphone/keymap.c
+++ keyboards/boardwalk/keymaps/eruphone/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
  * | CTRL   |   A    |   S    |   D    |   F    |   G    |   H    |   J    |   K    |   L    |  ;/+   |  :/＊  |  ]/}   | KC_ENT |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | SHIFT  |   Z    |   X    |   C    |   V    |   B    |   N    |   M    |  ,/<   |  ./>   |  //?   | SHIFT  |   ↑    |   _    |
+ * | SHIFT  |   Z    |   X    |   C    |   V    |   B    |   N    |   M    |  ,/<   |  ./>   |  //?   |   _    |   ↑    | SHIFT  |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
  * | FN     | OPT    | COMMAND| EN     |      SPACE      |      ENTER      |  KANA  | COMMAND|  FN    |   ←    |   ↓    |   →    |
  * '-----------------------------------------------------------------------------------------------------------------------------'
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC , KC_1   , KC_2   , KC_3    , KC_4   , KC_5   , KC_6   , KC_7   , KC_8    , KC_9   , KC_0   , KC_MINUS, JP_CIRC, KC_BSPC, \
   KC_TAB , KC_Q   , KC_W   , KC_E    , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I    , KC_O   , KC_P   , JP_AT   , JP_LBRC, JP_YEN , \
   KC_LCTL, KC_A   , KC_S   , KC_D    , KC_F   , KC_G   , KC_H   , KC_J   , KC_K    , KC_L   , KC_SCLN, JP_COLN , JP_RBRC, KC_ENT , \
-  KC_LSFT, KC_Z   , KC_X   , KC_C    , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM , KC_DOT , KC_SLSH, KC_RSFT , KC_UP  , JP_BSLS, \
+  KC_LSFT, KC_Z   , KC_X   , KC_C    , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM , KC_DOT , KC_SLSH, JP_BSLS , KC_UP  , KC_RSFT, \
   MO(_FN), KC_LALT, KC_LGUI, JP_MEISU,      KC_SPC     ,      KC_ENT     , JP_MKANA, KC_RGUI, MO(_FN), KC_LEFT , KC_DOWN, KC_RGHT  \
  ),
 
