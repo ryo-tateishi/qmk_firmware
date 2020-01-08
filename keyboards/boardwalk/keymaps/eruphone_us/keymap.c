01a1664ac00f471a0041523c750c8609ed8532d5
ryo-tateishi <ryo-tateishi@bm-sms.co.jp>
2020-01-08 19:45:27 +0900

    ダブルクォートが正しく認識されない問題を解消する

--- keyboards/boardwalk/keymaps/eruphone_us/keymap.c
+++ keyboards/boardwalk/keymaps/eruphone_us/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * | TAB    | Q      | W      | E      | R      | T      | Y      | U      | I      | O      | P      | [      | ]      | \      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | CTRL   | A      | S      | D      | F      | G      | H      | J      | K      | L      | ;      | "      | ENTER  | HOME   |
+ * | CTRL   | A      | S      | D      | F      | G      | H      | J      | K      | L      | ;      | '      | ENTER  | HOME   |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * | LSHIFT | Z      | X      | C      | V      | B      | N      | M      | ,      | .      | /      | SHIFT  | UP     | END    |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+--------+--------+--------|
@@ -27,8 +27,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
  [_DEFAULT] = LAYOUT_2u_arrow(
   KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS, KC_EQL,   KC_BSPC, \
-  KC_TAB  , KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC,  JP_YEN , \
-  KC_LCTL , KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_BSLS, KC_ENT,   KC_HOME, \
+  KC_TAB  , KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC,  KC_BSLS, \
+  KC_LCTL , KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_ENT,   KC_HOME, \
   KC_LSFT , KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,    KC_END , \
   MO(_FN) , KC_LALT, KC_LGUI, JP_MEISU,    KC_SPC,          KC_ENT,        JP_MKANA, KC_RGUI, MO(_FN), KC_LEFT, KC_DOWN,  KC_RGHT  \
  ),
